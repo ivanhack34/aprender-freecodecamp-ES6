443b03741151702c1b20fbf567204c21b8f937bc
ivanhack34 <48842215+ivanhack34@users.noreply.github.com>
2022-12-26 14:55:07 -0400

    Ejercicio del dia 10: 26-12-2022
    Ejercicio del modulo 2 - Del 1 al 6

---
+++ ejercicios.js
@@ -0,0 +1,95 @@
+//todo: Ejercicio del dia 10: 26/12/2022
+
+//?1-Compara el alcance de las palabras clave "var" y "let"
+
+/*
+function checkScope() {
+  let i = 'function scope';
+  if (true) {
+    let i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+
+console.log(checkScope());
+*/
+
+//?2-Muta un arreglo declarado con const
+
+/*
+const s = [5, 7, 2];
+function editInPlace() {
+  // Cambia solo el código debajo de esta línea
+
+  // Usar s = [2, 5, 7] sería inválido
+
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+  
+  return s;
+
+  // Cambia solo el código encima de esta línea
+}
+editInPlace();
+
+console.log(editInPlace());
+*/
+
+//?3-Prevenir la mutación del objeto
+
+/*
+function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Cambia solo el código debajo de esta línea
+    Object.freeze(MATH_CONSTANTS);
+  
+    // Cambia solo el código encima de esta línea
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
+
+  console.log(freezeObj());
+*/
+
+//?4-Usa funciones flecha para escribir funciones anónimas de manera breve
+
+/*
+const magic = () => {
+    return new Date();
+  };
+
+  console.log(magic());
+  */
+
+//?5-Escribe funciones flecha con parámetros
+
+/*
+const myConcat = (arr1, arr2) => {
+    return arr1.concat(arr2);
+  };
+  
+  console.log(myConcat([1, 2], [3, 4, 5]));
+  */
+
+//?6-Establece parámetros por defecto para tus funciones
+
+/*
+// Cambia solo el código debajo de esta línea
+const increment = (number, value = 1) => number + value;
+// Cambia solo el código encima de esta línea
+
+console.log(increment(2,5));
+console.log(increment(5));
+*/
+
+//?7-Utiliza el parámetro rest con parámetros de función
